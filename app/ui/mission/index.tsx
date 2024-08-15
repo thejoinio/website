0528ef604ce6a1e1f9cf577c22504b9cfc4d717f
Abdrahman Oladimeji <abdrahmanoladimeji02@gmail.com>
2024-08-16 00:54:26 +0100

    fix: missions, mission and modal in progress

--- app/ui/mission/index.tsx
+++ app/ui/mission/index.tsx
@@ -22,9 +22,115 @@ interface MissionProps {
   boldParts: string[];
 }
 
+function ButtonShapeLeft() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="25"
+      height="40"
+      fill="none"
+      viewBox="0 0 25 40"
+      className="inline-flex absolute -left-[25px] group-hover:translate-x-[0.2px] translate-x-[0.9px] h-10"
+    >
+      <path
+        fill="#1D1D1D"
+        fillOpacity="0.05"
+        d="M24.567 40c-2.434 0-4.692-.568-5.957-1.497L4.47 28.038c-5.401-3.998-5.401-12.079 0-16.076L18.61 1.496C19.875.567 22.133 0 24.567 0v40z"
+      ></path>
+      <path
+        fill="url(#paint0_linear_1_3190)"
+        fillOpacity="0.2"
+        d="M24.567 40c-2.434 0-4.692-.568-5.957-1.497L4.47 28.038c-5.401-3.998-5.401-12.079 0-16.076L18.61 1.496C19.875.567 22.133 0 24.567 0v40z"
+      ></path>
+      <defs>
+        <linearGradient
+          id="paint0_linear_1_3190"
+          x1="-5.101"
+          x2="28.903"
+          y1="8"
+          y2="11.655"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#8940FF"></stop>
+          <stop offset="1" stopColor="#66FECB"></stop>
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
+
+function ButtonShapeRight() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="25"
+      height="40"
+      fill="none"
+      viewBox="0 0 25 40"
+      className="inline-flex absolute -right-[25px] -translate-x-[0.7px] h-10"
+    >
+      <path
+        fill="#1D1D1D"
+        fillOpacity="0.05"
+        d="M.566 40c2.434 0 4.691-.568 5.957-1.497l14.14-10.465c5.401-3.998 5.401-12.079 0-16.076L6.523 1.496C5.257.567 3 0 .566 0v40z"
+      ></path>
+      <path
+        fill="url(#paint0_linear_1_3195)"
+        fillOpacity="0.2"
+        d="M.566 40c2.434 0 4.691-.568 5.957-1.497l14.14-10.465c5.401-3.998 5.401-12.079 0-16.076L6.523 1.496C5.257.567 3 0 .566 0v40z"
+      ></path>
+      <defs>
+        <linearGradient
+          id="paint0_linear_1_3195"
+          x1="23.708"
+          x2="-2.936"
+          y1="8"
+          y2="10.234"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#66FECB"></stop>
+          <stop offset="1" stopColor="#8940FF"></stop>
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
+
+const FollowBtn = ({ isDone, onClick }: { isDone: boolean, onClick: React.MouseEventHandler<HTMLButtonElement> | undefined }) => {
+  return (
+    <button
+    onClick={onClick}
+      style={{
+        background:
+          "linear-gradient(98deg, rgba(102, 254, 203, 0.20) 6.1%, rgba(137, 64, 255, 0.20) 103.66%), rgba(29, 29, 29, 0.05)",
+      }}
+      className="group relative inline-flex items-center justify-center whitespace-nowrap transition-all disabled:pointer-events-none disabled:opacity-50 hover:scale-95 mx-[12px] h-10 px-2 text-[15px] font-semibold text-white w-[86px]"
+    >
+      <ButtonShapeLeft />
+      {isDone ? (
+        <span className="mt-0.5">Done</span>
+      ) : (
+        <>
+          <span className="mt-0.5">Follow</span>{" "}
+          <OutlineSend className="ml-2 mt-1" />
+        </>
+      )}
+      <ButtonShapeRight />
+    </button>
+  );
+};
+
 export const Mission: React.FC<
-  MissionProps & { onFollowClick: () => void }
-> = ({ platform, icon, link, description, boldParts, onFollowClick }) => {
+  MissionProps & { onFollowClick: () => void; isMissionValidated: boolean }
+> = ({
+  platform,
+  icon,
+  link,
+  description,
+  boldParts,
+  onFollowClick,
+  isMissionValidated,
+}) => {
   return (
     <div className="flex px-4 md:px-0 mb-10">
       <div className="flex md:block w-[40px]">
@@ -63,10 +169,16 @@ export const Mission: React.FC<
         </div>
         <Button
           variant="shaped-gradient"
-          className="flex justify-center items-center"
+          className="justify-center items-center"
           onClick={onFollowClick}
         >
-          <span className="p-2">Follow </span> <OutlineSend />
+          {isMissionValidated ? (
+            <span className="min-w-[86px]">Done</span>
+          ) : (
+            <span className="flex items-center justify-center min-w-[86px] gap-2">
+              <span className="">Follow </span> <OutlineSend className="mt-0.5" />
+            </span>
+          )}
         </Button>
       </div>
       <div className="flex md:block w-[40px] -translate-x-[1px]">
--- app/ui/missions/index.tsx
+++ app/ui/missions/index.tsx
@@ -163,6 +163,29 @@ const mission2Data: MissionData[] = [
   },
 ];
 
+
+const TestBtn = () => {
+  return (
+    <button className="relative inline-flex items-center justify-center whitespace-nowrap transition-all disabled:pointer-events-none disabled:opacity-50 bg-[#F2E6E0] hover:scale-95 mx-[12px] h-9 px-2 text-lg font-bold text-dark w-[140px]">
+      <svg
+        viewBox="0 0 15 45"
+        fill="none"
+        className="inline-flex absolute left-[-11px] text-primary-foreground fill-[#F2E6E0] h-9"
+      >
+        <path d="M15 0H12.6085C11.4514 0 10.3974 0.665477 9.89991 1.7102L0.614192 21.2102C0.225634 22.0262 0.225634 22.9738 0.614191 23.7898L9.8999 43.2898C10.3974 44.3345 11.4514 45 12.6085 45H15V0Z"></path>
+      </svg>
+      <span className="mt-1">START</span>
+      <svg
+        viewBox="0 0 15 45"
+        fill="none"
+        className="inline-flex absolute right-[-11px] text-primary-foreground fill-[#F2E6E0] h-9"
+      >
+        <path d="M0 0H2.39151C3.54864 0 4.60261 0.665477 5.10009 1.7102L14.3858 21.2102C14.7744 22.0262 14.7744 22.9738 14.3858 23.7898L5.1001 43.2898C4.60261 44.3345 3.54864 45 2.39151 45H0V0Z"></path>
+      </svg>
+    </button>
+  );
+};
+
 export const checkAllMissionsCompleted = (
   validatedMissions: Record<string, boolean>,
 ) => {
@@ -173,6 +196,7 @@ export const checkAllMissionsCompleted = (
   }
   return true;
 };
+
 
 export const Missions = () => {
   const STORAGE_KEY = "validatedMissions";
@@ -205,6 +229,17 @@ export const Missions = () => {
       }
     }
   }, []);
+
+  const isMissionValidated = (missionKey: string) => {
+    if (typeof window !== "undefined") {
+      const storedMissions = localStorage.getItem(STORAGE_KEY);
+      if (storedMissions) {
+        const parsedValidatedMissions = JSON.parse(storedMissions);
+        return parsedValidatedMissions[missionKey] || false;
+      }
+    }
+    return false;
+  };
 
   const handleFollowClick = (missionKey: string) => {
     setCurrentMissionKey(missionKey);
@@ -326,6 +361,7 @@ export const Missions = () => {
             {missionData.map((data, idx) => (
               <Mission
                 onFollowClick={() => handleFollowClick(`Mission-${data.id}`)}
+                isMissionValidated={isMissionValidated(`Mission-${data.id}`)}
                 key={idx}
                 {...data}
               />
@@ -376,14 +412,14 @@ export const Missions = () => {
             ) : (
               <form
                 onSubmit={handleSubmit}
-                className="bg-[#7F5CE2] py-3 sm:py-4 px-3 sm:px-[30px] flex items-center w-full rounded-[20px] mt-24"
+                className="bg-[#7F5CE2] py-3 sm:py-4 px-3 sm:px-4 flex items-center justify-between w-full rounded-[20px] mt-24"
               >
                 <input
                   type="text"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                   placeholder="Enter your email"
-                  className="bg-transparent py-0 sm:py-2 sm:bg-[rgba(171,147,241,0.50)] rounded-xl border-none outline-none text-white placeholder:text-white font-medium text-sm sm:text-2xl w-[95%] sm:w-[90%] px-0 sm:px-4"
+                  className="bg-transparent py-0 sm:py-2 sm:bg-[rgba(171,147,241,0.50)] rounded-xl border-none outline-none text-white placeholder:!text-white font-medium text-sm sm:text-2xl w-[95%] sm:w-[90%] max-w-[548px] px-0 sm:px-4"
                 />
                 <Button
                   type="submit"
