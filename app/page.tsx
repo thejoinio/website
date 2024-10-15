6982675e72125b931114b0eeddf4a2084ddb6ff9
Abdrahman Oladimeji <abdrahmanoladimeji02@gmail.com>
2024-10-15 09:37:16 +0100

    udpate: added 10 days to the countdown

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
+import React from "react";
 import { Marquee } from "@/app/ui/marquee";
 import { Hero } from "@/app/ui/hero";
 import { Possibilities } from "@/app/ui/possibilities";
 import { Ecosystem } from "@/app/ui/ecosystem";
 import { MarqueeSection } from "@/app/ui/marquee-section";
-import React from "react";
 import { PresaleSection } from "@/app/ui/presale-section";
 import { FAQSSection } from "@/app/ui/faqs-section";
 
