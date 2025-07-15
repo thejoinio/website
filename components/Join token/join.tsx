4ea7112ef3a329c0684908e7cda3046c0c11bd4e
ismaila Jatau <izmaxp07@gmail.com>
2025-07-15 15:55:35 +0100

    delete an image

--- components/Join token/join.tsx
+++ components/Join token/join.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Image from "next/image";
-import join from "@/assets/images/join.png";
 import join2 from "@/assets/images/joinx.png";
 
 const JoinTokenComponent = () => {
   return (
+    
     <div className="Join-token text-center">
       <h1 className="text-3xl font-bold p-[16px]">JOIN Token</h1>
       <div className="flex justify-center items-center my-4">
-        <div className="text-lg">
+        <div className="text-lg ">
           <p>The JOIN Token is the native utility token powering the</p>
           <p>entire Joinda ecosystem. Built on the Solana</p>
           <p>blockchain, it facilitates fast, low-cost, and secure</p>
@@ -17,7 +17,7 @@ const JoinTokenComponent = () => {
         </div>
 
         <div className="flex w-18" >
-         
+         {/* addin pictiure to the main  */}
           <Image
             src={join2}
             alt="image of JOIN token"
