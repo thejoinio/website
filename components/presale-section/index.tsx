12bf3c14288f5b3f3b2c6f8209d1820912072e83
Abdrahman Oladimeji <abdrahmanoladimeji02@gmail.com>
2025-07-17 19:07:58 +0100

    uncompleted merge

--- components/presale-section/index.tsx
+++ components/presale-section/index.tsx
@@ -1,27 +1,4 @@
 import Image from "next/image";
-<<<<<<< HEAD
-import demigods from '@/assets/images/demigods-nft-cards.png'
-import card from "@/assets/images/crypto-debit-card.png";
-import { Button } from "../button";
-
-export default function PresaleSection () {
-    return (
-        <section className="presale-section overflow-hidden py-[60px] md:py-[52px] relative">
-            <div className="flex flex-col-reverse md:flex-row items-center gap-[60px] relative w-full max-w-[1440px] mx-auto px-5 md:px-10 lg:px-20">
-                <div className="flex justify-center items-center w-full md:w-1/2">
-                    <Image src={card} alt="3 Demigods NFT Cards" width={1651} height={1502} className="w-full max-w-[550px] z-10" />
-                </div>
-                <div className="flex flex-col gap-5 xs:gap-[30px] w-full md:w-1/2 z-10">
-                    <h4 className="text-2xl/normal xs:text-4xl/normal md:text-5xl/normal">Grab your Spot in the JOIN Presale.</h4>
-                    <p className="text-base xs:text-xl md:text-2xl">Exclusive Freebies Await!</p>
-                    <Button variant="dark" className="text-xl px-[30px] py-[15px]" disabled>Join Presale</Button>
-                </div>
-                <div className="absolute left-0 h-full w-full bg-presaleSectionBg bg-right-top md:bg-right bg-no-repeat mix-blend-soft-light"></div>
-            </div>
-        </section>
-    )
-}
-=======
 import cryptoDebitCard from "@/assets/images/crypto-debit-card-big.png";
 import { Button } from "../button";
 
@@ -149,4 +126,3 @@ export default function PresaleSection() {
     </section>
   );
 }
->>>>>>> f9a7537 (feat: new updates ✨✅)
